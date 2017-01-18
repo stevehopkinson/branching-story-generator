1f3ebbad5d437aab22ec958884cfc94f561c2edd
Steve Hopkinson <steve@stevehopkinson.co.uk>
2017-01-18 20:21:44 +0000

    Create story object and helper functions

--- src/app.js
+++ src/app.js
@@ -10,8 +10,24 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(serveStatic('public'));
 
+// Create story object and helper functions
+const story = [{ text: "There once was a big, bad wolf." }];
+
+const getNode = nodeId => story[nodeId];
+
+const getChildId = (parentId, compass) => getNode(parentId)[compass];
+
+const createNode = ({ text, parentId, compass }) => {
+  let parentNode = getNode(parentId),
+      newNodeIndex = story.length,
+      newNode = { text };
+
+  nodes.push(newNode);
+  parentNode[compass] = newNodeIndex;
+};
+
 // Configure routes
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render('index', story[0]));
 
 // Export app
 module.exports = app;

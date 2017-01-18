ab96659933f9f18f1604e47e08d5080bd4fa2eb0
Steve Hopkinson <steve@stevehopkinson.co.uk>
2017-01-18 20:11:12 +0000

    Install and configure serve-static module

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const serveStatic = require('serve-static');
 
 // Instantiate app and configure middleware
 const app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(serveStatic('public'));
 
 // Configure routes
 app.get('/', (req, res) => res.render('index'));
